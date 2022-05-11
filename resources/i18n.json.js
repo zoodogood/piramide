e55d39656922caeb816977d3afb087760c8b2834
zoodogood <52154209+zoodogood@users.noreply.github.com>
2022-05-11 19:06:49 +0300

    Some-main content transtlated

--- resources/i18n.json.js
+++ resources/i18n.json.js
@@ -1,6 +1,16 @@
 globalThis.i18nLanguages = {
   ua: {
     "codeSpace-controlButtons-play": "Грати",
+    "codeSpace-controlButtons-scoreMap": "Графік эфектив.",
+    "codeSpace-controlButtons-copy": "Копіювати",
+    "codeSpace-controlButtons-showConsole": "Консоль",
+    "codeSpace-controlButtons-setDefault": "Відновити код",
+    "codeSpace-controlButtons-showHelps": "Біблиотека",
+
+    "footer-article-h1": "Пояснення задачі",
+
+    "footer-advice-h2": "Прочитайте",
+    "footer-advice-settings": "Змініть налаштування",
 
     "settings-action-lang-h2": "Вибрана Мова",
     "settings-action-lang-en": "Англійська",
@@ -15,6 +25,16 @@ globalThis.i18nLanguages = {
 
   ru: {
     "codeSpace-controlButtons-play": "Играть!",
+    "codeSpace-controlButtons-scoreMap": "График эффектив.",
+    "codeSpace-controlButtons-copy": "Скопировать",
+    "codeSpace-controlButtons-showConsole": "Консоль",
+    "codeSpace-controlButtons-setDefault": "Код по умолчанию",
+    "codeSpace-controlButtons-showHelps": "Библиотека",
+
+    "footer-article-h1": "Пояснение задачи",
+
+    "footer-advice-h2": "Прочтите",
+    "footer-advice-settings": "Измените настройки",
 
     "settings-action-lang-h2": "Выбранный Язык",
     "settings-action-lang-en": "Английский",
@@ -30,6 +50,16 @@ globalThis.i18nLanguages = {
 
   en: {
     "codeSpace-controlButtons-play": "Play",
+    "codeSpace-controlButtons-scoreMap": "Show graph",
+    "codeSpace-controlButtons-copy": "Copy",
+    "codeSpace-controlButtons-showConsole": "Console",
+    "codeSpace-controlButtons-setDefault": "Reset code",
+    "codeSpace-controlButtons-showHelps": "Library",
+
+    "footer-article-h1": "Task info",
+
+    "footer-advice-h2": "You! Read me! NOW!",
+    "footer-advice-settings": "Change Settings",
 
     "settings-action-lang-h2": "Selected Language",
     "settings-action-lang-en": "English",
